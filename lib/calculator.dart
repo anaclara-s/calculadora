db385c7ba1efb8b9fc0aaf8b9f7110c8b0383603
Ana <anaclarasantos.amorin@gmail.com>
2023-10-03 01:49:22 -0300

    adicionando List e Constants

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,16 +1,63 @@
+import 'package:calculadora/constants.dart';
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
+import 'list_buttons.dart';
+
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  var screenOperation = '';
+  var screenResult = '';
+  var previus = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('tela'),
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(kEdgeInsets),
+            alignment: Alignment.centerRight,
+            child: Text(
+              screenOperation,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(kEdgeInsets),
+            alignment: Alignment.centerRight,
+            child: Text(
+              screenResult,
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.pink,
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemCount: buttons.length,
+                itemBuilder: ((BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.all(10),
+                  );
+                }),
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final bgPrimaryColor = Color.fromARGB(255, 0, 102, 255);
+final bgSecundaryColor = Color.fromARGB(255, 119, 0, 255);
+final hingLigthColor = Color.fromARGB(255, 0, 255, 128);
+double kEdgeInsets = 20;
